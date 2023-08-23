d8e263027c6ac6ee633e0b2cb486b381f95fab06
Matt Garry <mhgarry92@gmail.com>
2023-08-23 17:39:37 -0400

    start webpack config/eslint

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,30 +1,45 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //generates my html file
+const WebpackPwaManifest = require('webpack-pwa-manifest'); //to build my mainfest file with 
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const { InjectManifest } = require('workbox-webpack-plugin'); //to inject my manifest into my application
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'development', 
+    // entry point 
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './src/js/index.js', //sets index.js as the main entry point to be read from 
+      install: './src/js/index.js', //sets index.js as the point to install service worker
     },
+    // output for pwa bundle
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
-};
+      filename: '[name].bundle.js', //our bundle output file
+      path: path.resolve(__dirname, 'dist'), //outputs our bundle.js file to a created 'dist' file
+     },
+     //plugins for webpack
+     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }), //creating an HTML file for bundle titled 'JATE'
+      new WebpackPwaManifest({ //creating a manifest.json file with the following configurations
+        fingerprints: false,
+        inject: true, //inject service worker into bundle
+        name: 'JATE', //name bundle 'JATE'
+        short_name: 'JATE',
+        description: 'Just Another Text Editor', //describe bundle as Just Another Text Editor
+        background_color: '#225ca3', //set color
+        theme_color: '#225ca3', //set tcolor
+        start_url: '/', //specify entry point
+        publicPath: '/', //specify publish point
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'), //pull logo src from src/images/logo.png
+            sizes: [96, 128, 192, 256, 284, 512], //specify sizes that logo can be
+            destination: path.join('asssets', 'icons') //destination folder icons under asssets where logo will be in final bundle
+          }
+        ]
+      })
+     ]
+  }
+}
