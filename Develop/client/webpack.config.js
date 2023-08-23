a6b0d7b8c6f94c388f330b1fabbc3e31e5900989
Matt Garry <mhgarry92@gmail.com>
2023-08-23 17:48:58 -0400

    fiinished and refactored webpack config file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,45 +1,67 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //generates my html file
-const WebpackPwaManifest = require('webpack-pwa-manifest'); //to build my mainfest file with 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Generates HTML file
+const WebpackPwaManifest = require('webpack-pwa-manifest'); // Generates manifest file
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin'); //to inject my manifest into my application
+const { InjectManifest } = require('workbox-webpack-plugin'); // Injects service worker into the application
 
 module.exports = () => {
   return {
     mode: 'development', 
-    // entry point 
     entry: {
-      main: './src/js/index.js', //sets index.js as the main entry point to be read from 
-      install: './src/js/index.js', //sets index.js as the point to install service worker
+      main: './src/js/index.js', // Main entry point for the application logic
+      install: './src/js/service-worker.js', // Entry point for service worker installation
     },
-    // output for pwa bundle
     output: {
-      filename: '[name].bundle.js', //our bundle output file
-      path: path.resolve(__dirname, 'dist'), //outputs our bundle.js file to a created 'dist' file
-     },
-     //plugins for webpack
-     plugins: [
+      filename: '[name].bundle.js', // Output bundle file name
+      path: path.resolve(__dirname, 'dist'), // Output directory path
+    },
+    plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE',
-      }), //creating an HTML file for bundle titled 'JATE'
-      new WebpackPwaManifest({ //creating a manifest.json file with the following configurations
-        fingerprints: false,
-        inject: true, //inject service worker into bundle
-        name: 'JATE', //name bundle 'JATE'
-        short_name: 'JATE',
-        description: 'Just Another Text Editor', //describe bundle as Just Another Text Editor
-        background_color: '#225ca3', //set color
-        theme_color: '#225ca3', //set tcolor
-        start_url: '/', //specify entry point
-        publicPath: '/', //specify publish point
+        template: './index.html', // HTML template file for HtmlWebpackPlugin
+        title: 'JATE', // Title for the HTML file
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false, // Don't use fingerprints in file names
+        inject: true, // Inject service worker into bundle
+        name: 'JATE', // Name of the PWA
+        short_name: 'JATE', // Short name of the PWA
+        description: 'Just Another Text Editor', // Description of the PWA
+        background_color: '#225ca3', // Background color for the PWA
+        theme_color: '#225ca3', // Theme color for the PWA
+        start_url: '/', // Entry point URL
+        publicPath: '/', // Public base path for assets
         icons: [
           {
-            src: path.resolve('src/images/logo.png'), //pull logo src from src/images/logo.png
-            sizes: [96, 128, 192, 256, 284, 512], //specify sizes that logo can be
-            destination: path.join('asssets', 'icons') //destination folder icons under asssets where logo will be in final bundle
-          }
-        ]
-      })
-     ]
-  }
-}
+            src: path.resolve('src/images/logo.png'), // Path to logo image
+            sizes: [96, 128, 192, 256, 384, 512], // Icon sizes
+            destination: path.join('assets', 'icons'), // Destination folder for icons
+          },
+        ],
+      }),
+      new InjectManifest({ // Inject service worker manifest
+        swSrc: './src-sw.js', // Source file for service worker
+        swDest: 'src-sw.js', // Destination for service worker in the build
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          // CSS loader configuration
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'], 
+        },
+        {
+          // Babel loader configuration for JavaScript
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader', // Use Babel to load and transform ES6
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          }, 
+        },
+      ],
+    },
+  };
+};
